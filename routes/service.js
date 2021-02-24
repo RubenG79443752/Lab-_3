1bb4bce6c9281517f962241f2840f050f6aa25b0
RubenM <fab79443752@gmail.com>
2021-02-24 03:29:25 -0400

    first commit

---
+++ routes/service.js
@@ -0,0 +1,6 @@
+import express from "express";
+import ServicesController from "../controller/ServicesController.js";
+var services = new ServicesController();
+router.get("/", services.index);
+router.get("/test", services.test);
+export default router;
